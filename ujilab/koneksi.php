cda2970c319ff2fdc5240077c4a879b88cd16ce1
farros01 <ilhamfaros@gmail.com>
2024-08-13 22:26:03 +0700

    Delete ujilab directory

--- ujilab/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn= mysqli_connect("localhost","root","","db_sinar_jaya");
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-date_default_timezone_set('Asia/Jakarta');   
-?>
